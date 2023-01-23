59e004e4951bf4bf0b2b1d4808c234d3740a19cc
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2023-01-23 12:15:03 +0100

    Added time component

--- dashboard-react-project/src/Components/Time.jsx
+++ dashboard-react-project/src/Components/Time.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 export default function Time() {
-  return (
-	<div>Time</div>
-  )
+
+	const [time, setTime] = useState(new Date())
+
+	useEffect(() => {
+		let timer = setInterval(() => setTime(new Date()), 1000)
+
+		return function cleanup() {
+			clearInterval(timer)
+		}
+	})
+	return (
+		<div>
+			<div>{time.toLocaleTimeString("en-GB", { timeStyle: "short" })}</div>
+			<div className='date'>{time.toLocaleDateString("en-GB", { year: "numeric", month: "long", day: "numeric" })}</div>
+		</div>
+	)
 }
