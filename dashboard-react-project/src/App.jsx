729d4036168ae003df3187af191f97428fef8e17
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2023-01-24 10:25:38 +0100

    Added weather component

--- dashboard-react-project/src/App.jsx
+++ dashboard-react-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
 import Weather from './Components/Weather';
--- dashboard-react-project/src/Components/Weather.jsx
+++ dashboard-react-project/src/Components/Weather.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react';
 
 export default function Weather() {
-  return (
-	<div>Weather</div>
-  )
+	const [weather, setWeather] = useState({
+		temperature: "",
+		icon: "",
+		city: ""
+	})
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(position => {
+			console.log(position)
+			fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather/?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+				.then(res => {
+					if (!res.ok) {
+						throw Error("Weather data not available")
+					}
+					return res.json()
+				})
+				.then(data => {
+					console.log(data)
+					setWeather({
+						temperature: Math.round(data.main.temp),
+						icon: data.weather[0].icon,
+						city: data.name
+					})
+				})
+				.catch(err => console.error(err))
+		})
+	}, [])
+
+	return (
+		<div className='weather'>
+			<div className='weather-temp-icon'>
+				{weather.icon&&<img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} />}
+				{weather.temperature&&<span>{weather.temperature}°C</span>}
+			</div>
+			<div className='weather-city'>{weather.city}</div>
+		</div>
+	)
 }
